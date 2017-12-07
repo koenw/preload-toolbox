e461d37ecd4f32f278ff8655107e13cb4c060cf3
Koen Wilde <koen@chillheid.nl>
2017-12-07 11:41:21 +0100

    Specify mode for logfile
    Also improve log messages.

--- src/ssl.c
+++ src/ssl.c
@@ -23,19 +23,20 @@ int SSL_read(SSL *ssl, void *buf, int num) {
     return ret;
   }
 
-  int f = open(log_file, O_WRONLY|O_APPEND|O_CREAT);
+  mode_t mode = S_IRUSR | S_IWUSR;
+  int f = open(log_file, O_WRONLY|O_APPEND|O_CREAT, mode);
   if (f == -1) {
-    perror("Failed to open logfile");
+    perror("[preload-ssl]: Failed to open logfile");
     return ret;
   }
 
   if (write(f, buf, num) < 0) {
-    perror("Failed to write buffer to logfile");
+    perror("[preload-ssl]: Failed to write buffer to logfile");
     return ret;
   }
 
   if (close(f) < 0)
-    perror("Failed to close file");
+    perror("[preload-ssl]: Failed to close file");
 
   return ret;
 }
